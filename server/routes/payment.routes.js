d23f4ed896a02742e44034c5e6ca1d82ae286ba2
Karan Sahu <iamironman892465@gmail.com>
2026-06-21 14:58:39 +0530

    nameing modifiction

--- server/routes/payment.routes.js
+++ server/routes/payment.routes.js
@@ -2,7 +2,7 @@
 import express from "express";
 const router = express.Router()
 
-import {capturePayment,verifySignature,enrollStudents,sendPaymentSuccessEmail} from '../controller/payment.js'
+import {capturePayment,verifySignature,enrollStudents,sendPaymentSuccessEmail} from '../controller/payment.controller.js'
 import {auth , isStudent} from "../middleware/auth.middleware.js";
 router.post("/capturePayment", auth, isStudent, capturePayment)
 router.post("/verifySignature",auth, isStudent, verifySignature)
