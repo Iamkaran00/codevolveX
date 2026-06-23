28349adbc0ee69e80f8d54cbe5dffd1f23df9823
Karan Sahu <161184262+Iamkaran00@users.noreply.github.com>
2026-06-23 16:10:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json({limit : "4000mb"}));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://codevolvx.vercel.app",
     credentials: true,
   })
 );
